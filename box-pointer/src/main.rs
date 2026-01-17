cdff07e98be88a668d48f3de6438c92e14123efb
Abhijeet Ashok Muneshwar <openingknots@gmail.com>
2026-01-17 20:14:40 +0530

    🎨 Added 1 more example of Box Pointer
    This example shows show trait can have fix size

--- box-pointer/src/main.rs
+++ box-pointer/src/main.rs
@@ -5,6 +5,22 @@ enum List<T> {
 
 use List::{Cons, Nil};
 
+trait Vehicle{
+    fn drive(&self);
+}
+
+struct Truck;
+
+impl Vehicle for Truck{
+    fn drive(&self){
+        println!("Truck is driving");
+    }
+}
+
 fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let t : Box<dyn Vehicle>;
+    t = Box::new(Truck);
+    t.drive();
 }
